b853380ffc7fa53f953cc50b1546a311ddab0163
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-09-09 18:25:37 -0300

    Implementing more of the list pessoa component

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasicSearchComponent } from './basic-search/basic-search.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [BasicSearchComponent],
+  exports: [BasicSearchComponent]
 })
 export class SharedComponentsModule { }
