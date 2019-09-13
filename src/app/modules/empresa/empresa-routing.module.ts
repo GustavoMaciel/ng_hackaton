a2967fb2999017e6b2b0242f031006caed9df0b7
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-09-13 18:51:13 -0300

    Adding routes to module empresa

--- src/app/modules/empresa/empresa-routing.module.ts
+++ src/app/modules/empresa/empresa-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmpresaListComponent } from './components/empresa-list/empresa-list.component';
+import { EmpresaEditComponent } from './components/empresa-edit/empresa-edit.component';
+import { EmpresaViewComponent } from './components/empresa-view/empresa-view.component';
 
 const routes: Routes = [
-  { path: '', component: EmpresaListComponent }
+  { path: '', component: EmpresaListComponent },
+  { path: 'create', component: EmpresaEditComponent, data: {breadcrumb: 'Create'} },
+  { path: 'edit/:id', component: EmpresaEditComponent, data: {breadcrumb: 'Edit'} },
+  { path: 'view/:id', component: EmpresaViewComponent, data: {breadcrumb: 'View'} }
 ];
 
 @NgModule({
--- src/app/modules/empresa/empresa.module.ts
+++ src/app/modules/empresa/empresa.module.ts
@@ -4,12 +4,17 @@ import { EmpresaListComponent } from './components/empresa-list/empresa-list.com
 import { EmpresaEditComponent } from './components/empresa-edit/empresa-edit.component';
 import { EmpresaViewComponent } from './components/empresa-view/empresa-view.component';
 import { EmpresaRoutingModule } from './empresa-routing.module';
+import { EmpresaService } from './services/empresa.service';
+import { EmpresaMockService } from './services/empresa.mock.service';
 
 @NgModule({
   declarations: [EmpresaListComponent, EmpresaEditComponent, EmpresaViewComponent],
   imports: [
     CommonModule,
     EmpresaRoutingModule
+  ],
+  providers: [
+    {provide: EmpresaService, useClass: EmpresaMockService}
   ]
 })
 export class EmpresaModule { }
