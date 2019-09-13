cab91e7c79d2d00816ab6849c5dc13905c0cc707
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-09-13 19:13:32 -0300

    T2.12, 2.13

--- src/app/modules/empresa/components/empresa-view/empresa-view.component.ts
+++ src/app/modules/empresa/components/empresa-view/empresa-view.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from '../../empresa';
+import { ActivatedRoute } from '@angular/router';
+import { EmpresaService } from '../../services/empresa.service';
+import { BaseComponent } from 'src/app/shared/shared-components/base.component';
 
 @Component({
   selector: 'app-empresa-view',
   templateUrl: './empresa-view.component.html',
   styleUrls: ['./empresa-view.component.scss']
 })
-export class EmpresaViewComponent implements OnInit {
+export class EmpresaViewComponent extends BaseComponent {
 
-  constructor() { }
+  company: Empresa;
+
+  constructor(private route: ActivatedRoute, private service: EmpresaService) { super(); }
 
   ngOnInit() {
+    super.ngOnInit();
+    const id: number = +this.route.snapshot.paramMap.get("id");
+    this.setCompany(id);
+  }
+
+  /**
+   * Set the company variable to a Empresa object with the informed ID
+   * 
+   * @param {number} id 
+   */
+  setCompany(id: number): void{
+    this.service.getById(id).subscribe((result) => {
+      this.company = result;
+    });
   }
 
 }
