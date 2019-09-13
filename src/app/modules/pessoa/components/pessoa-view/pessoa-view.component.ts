2ce72f71306245cf50192150331789dfea21e1ac
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-09-12 22:01:17 -0300

    Implementing T1.12, T1.13

--- src/app/modules/pessoa/components/pessoa-view/pessoa-view.component.ts
+++ src/app/modules/pessoa/components/pessoa-view/pessoa-view.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/shared/shared-components/base.component';
+import { Pessoa } from '../../pessoa';
+import { ActivatedRoute } from '@angular/router';
+import { PessoaService } from '../../services/pessoa.service';
 
 @Component({
   selector: 'app-pessoa-view',
   templateUrl: './pessoa-view.component.html',
   styleUrls: ['./pessoa-view.component.scss']
 })
-export class PessoaViewComponent implements OnInit {
+export class PessoaViewComponent extends BaseComponent {
 
-  constructor() { }
+  person: Pessoa;
+
+  constructor(private route: ActivatedRoute, private service: PessoaService) {
+    super();
+   }
 
   ngOnInit() {
+    super.ngOnInit();
+    const id: number = +this.route.snapshot.paramMap.get("id");
+    this.setPessoa(id);
+  }
+
+  /**
+   * Set the pessoa var into a Pessoa object with the informed id
+   * 
+   * @param {number} id 
+   */
+  setPessoa(id: number): void{
+    this.service.getById(id).subscribe((result) => {
+      this.person = result;
+    });
   }
 
 }
